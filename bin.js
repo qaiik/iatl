a034045beaaed34efb64273c3d87c7e4bdfc0f82
qaiik <82181231+qaiik@users.noreply.github.com>
2022-07-30 23:57:09 -0600

    Update bin.js

--- bin.js
+++ bin.js
@@ -3,17 +3,32 @@ const locateSFD = require("./utils/locateSFD.js");
 const parseDependencies = require("./utils/parseDeps.js");
 const install = require("./utils/installPackage.js");
 
+const fs = require("fs");
 async function main() {
-  const dependencies = await locateSFD();
+  let dependencies = await locateSFD();
   
   for (const dependency of dependencies) {
     const parsed = parseDependencies(dependency);
     
     const Latest = await latest(parsed.packageName);
-    if (Latest !== parsed.version && !parsed.unsafeInstalls) {
-      await install(parsed.packageName, Latest);
-    } else {
-      await install(parsed.packageName, parsed.version)
+    if (parsed.unsafeInstalls) {
+      console.log("UNSAFE getting ", parsed.packageName, parsed.version)
+      await install(parsed.packageName, parsed.version);
+    } 
+    
+    if (!parsed.unsafeInstalls && parsed.version !== Latest) {
+      console.log("UPDATE", parsed.packageName, parsed.version, "->", Latest)
+      await install(parsed.packageName, Latest)
+
+      dependencies = dependencies.map(dep => {
+        if (dep === dependency) {
+          return `${parsed.packageName} v: ${Latest}`
+        }
+
+        return dep
+      })
+
+      fs.writeFileSync("./sfpack.json", JSON.stringify(dependencies, null, 4));
     }
     
   }
