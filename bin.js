5016a0bf3e2ad814d5abeae098b1af5c4b188a21
Logan N <selduaa2@gmail.com>
2022-07-31 00:07:32 -0600

    updated all

--- bin.js
+++ bin.js
@@ -1,24 +1,40 @@
+#!/usr/bin/env node
 const { latest } = require("./utils/getLatestVersion.js");
-const locateSFD = require("./utils/parseSFD.js");
+const locateSFD = require("./utils/locateSFD.js");
 const parseDependencies = require("./utils/parseDeps.js");
 const install = require("./utils/installPackage.js");
 
+const fs = require("fs");
 async function main() {
-  const dependencies = await locateSFD();
+  let dependencies = await locateSFD();
   
   for (const dependency of dependencies) {
     const parsed = parseDependencies(dependency);
     
     const Latest = await latest(parsed.packageName);
-    if (Latest !== parsed.version && !parsed.unsafeInstalls) {
-      await install(parsed.packageName, Latest);
-    } else {
-      await install(parsed.packageName, parsed.version)
+    if (parsed.unsafeInstalls) {
+      console.log("UNSAFE getting ", parsed.packageName, parsed.version)
+      await install(parsed.packageName, parsed.version);
+    } 
+    
+    if (!parsed.unsafeInstalls && parsed.version !== Latest) {
+      console.log("UPDATE", parsed.packageName, parsed.version, "->", Latest)
+      await install(parsed.packageName, Latest)
+
+      dependencies = dependencies.map(dep => {
+        if (dep === dependency) {
+          return `${parsed.packageName} v: ${Latest}`
+        }
+
+        return dep
+      })
+
+      fs.writeFileSync("./sfpack.json", JSON.stringify(dependencies, null, 4));
     }
     
   }
 }
 
 main().then(() => {
-  console.log("INFO installations started")
+  console.log("INFO installations over")
 })
