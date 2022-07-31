e116e292b34296176ba5833774be463cb895bac6
qaiik <82181231+qaiik@users.noreply.github.com>
2022-07-30 22:30:25 -0600

    main code, !extras

--- bin.js
+++ bin.js
@@ -1,2 +1,24 @@
 const { latest } = require("./utils/getLatestVersion.js");
+const locateSFD = require("./utils/parseSFD.js");
+const parseDependencies = require("./utils/parseDeps.js");
+const install = require("./utils/installPackage.js");
 
+async function main() {
+  const dependencies = await locateSFD();
+  
+  for (const dependency of dependencies) {
+    const parsed = parse(dependency);
+    
+    const Latest = await latest(parsed.packageName);
+    if (Latest !== parsed.version && !parsed.unsafeInstalls) {
+      await install(parsed.packageName, Latest);
+    } else {
+      await install(parsed.packageName, parsed.version)
+    }
+    
+  }
+}
+
+main().then(() => {
+  console.log("INFO installations started")
+})
